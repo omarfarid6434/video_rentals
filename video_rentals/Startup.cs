8ef929fa5e5e1fffdecafd666411d54f32d9ce28
Frarid Hossain <omarfarid778@gmail.com>
2018-02-28 05:36:05 +0600

    Add project files.

---
+++ video_rentals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(video_rentals.Startup))]
+namespace video_rentals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
